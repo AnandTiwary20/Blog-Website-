840cb6b3887ea54d2ce63901a0dd389b83223f4e
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-08-26 00:28:24 +0530

    Final checking done and working checked

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function FooterComp() {
             <div className="mt-5">
             <Link to ="/" className='self-center whitespace-nowrap 
             text-lg sm:text-xl font-semibold dark:text-white'>
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Rishabh's</span>
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Anand's</span>
               Blogify
             </Link>
             </div>
@@ -30,7 +30,7 @@ export default function FooterComp() {
                       target='_blank'
                       rel='noopener noreferrer'
                     >
-                        Rishabh's Blogify
+                        Anand's Blogify
                     </Footer.Link>
                    </Footer.LinkGroup>
                 </div>
@@ -38,14 +38,14 @@ export default function FooterComp() {
                    <Footer.Title title='Follow us' />
                    <Footer.LinkGroup col>
                     <Footer.Link
-                       href='https://github.com/RishabhJain2404'
+                       href='#'
                        target='_blank'
                        rel='noopener noreferrer'
                     >
                         GitHub
                     </Footer.Link>
                     <Footer.Link
-                       href='https://www.linkedin.com/in/rishabhjain2404/'
+                       href='#'
                        target='_blank'
                        rel='noopener noreferrer'
                     >
@@ -66,15 +66,15 @@ export default function FooterComp() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright 
             href="#"
-            by="Rishabh's blogify"
+            by="Anand's blogify"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/RishabhJain2404' icon={BsGithub}/>
-            <Footer.Icon href='https://www.linkedin.com/in/rishabhjain2404/' icon={BsLinkedin}/>
+            <Footer.Icon href='#' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsLinkedin}/>
           </div>
         </div>
       </div>
